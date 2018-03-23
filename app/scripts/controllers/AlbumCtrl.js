f532fb4b885d379295660cc290524c42135771da
Ross Ginn <ross.ginn@gmail.com>
2018-03-22 17:35:22 -0700

    services 2

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,6 +1,7 @@
 (function() {
-  function AlbumCtrl(Fixtures) {
-     this.album = Fixtures.getAlbum();
+  function AlbumCtrl(Fixtures, SongPlayer) {
+         this.albumData = Fixtures.getAlbum();
+         this.songPlayer = SongPlayer;
  }
      angular
          .module('blocJams')
